84bcf13630ac99c9dc475f8404be656d0ccce401
b0pez <blopez4434@gmail.com>
2022-11-01 18:35:17 -0300

    mostrar imagenes

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs"
+import { GifItem } from "./GifItem";
 
 export const GifGrid = ({ category }) => {
 
-    getGifs(category);
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        getGifs(category)
+            .then(setImages);
+    }, []);
+
 
     return (
         <>
-            <h1>gola</h1>
+            <h3>{category}</h3>
+
+            <div className='card-grid'>
+                {
+                    images.map(({id, url, title}) => (
+                        <GifItem key={id} {...{title, url}} />
+                    ))
+                }
+            </div>
         </>
     )
 }
