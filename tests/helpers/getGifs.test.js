68966e6251b0455e0030b5aa24edcb200279cedb
b0pez <blopez4434@gmail.com>
2023-01-30 23:22:03 -0300

    prueba a getgifs

--- tests/helpers/getGifs.test.js
+++ tests/helpers/getGifs.test.js
@@ -6,5 +6,13 @@ describe('Pruebas en getGifs()', () => {
         const gifs = await getGifs('One Punch');
         //Que el arreglo tenga una longitud mayor a 0
         expect(gifs.length).toBeGreaterThan(0);
+
+        //Evaluamos que el Objeto tenga las propiedades que esperamos
+        expect(gifs[0]).toEqual({
+            id: expect.any(String),
+            title: expect.any(String),
+            url: expect.any(String)
+        });
+
      });
  });
